17aacd0a89890375f06ce75c4c21b6430af6f694
Daniel Meszaros <daniel@ECS0614.local>
2018-01-10 11:45:04 +0000

    let's build again

--- tf/scripts/user_data.sh
+++ tf/scripts/user_data.sh
@@ -7,7 +7,7 @@ export VERSION_COLOUR=${v_col}
 sudo echo 'APP_ENV=${env}' >> /etc/environment
 sudo echo 'VERSION_COLOUR=${v_col}' >> /etc/environment
 
-aws s3 cp --region eu-west-1 s3:///ecsd-mdanidl/go-demo/artifacts/${v_num}/go-demo .
+aws s3 cp --region eu-west-1 s3://ecsd-mdanidl/go-demo/artifacts/${v_num}/go-demo .
 sudo chmod +x ./go-demo
 sudo mv ./go-demo /usr/local/bin/go-demo
 
